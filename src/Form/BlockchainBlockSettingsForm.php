f99da69a656506cace1c59bfa8bc9e910757aff3
Olexyy <olexyy.mails@gmail.com>
2018-06-22 21:32:43 +0300

    https://github.com/Olexyy/blockchain/issues/5

--- src/Form/BlockchainBlockSettingsForm.php
+++ src/Form/BlockchainBlockSettingsForm.php
@@ -81,4 +81,15 @@ class BlockchainBlockSettingsForm extends FormBase {
     return $form;
   }
 
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
 }
